dc02f80310f3219bd11fd1b57a11e0b508eec6c8
AdilMulimani <adilmulimani@gmail.com>
2024-12-18 22:39:28 +0530

    feat(routes): add get available and unavailable routes to car-routes.js

--- routes/car-routes.js
+++ routes/car-routes.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const {registerCar,getAllCars,getCarById,updateCar,deleteCar} = require("../controllers/car/car-controller");
+const {registerCar,getAllCars,getCarById,updateCar,deleteCar,getAllAvailableCars,getAllNotAvailableCars} = require("../controllers/car/car-controller");
 const carRouter = express.Router();
 
 carRouter.post('/register', registerCar);
 carRouter.put('/update/:id', updateCar);
 carRouter.delete('/delete/:id', deleteCar);
 carRouter.get('/get-all-cars',getAllCars);
+carRouter.get('/get-all-available-cars',getAllAvailableCars);
+carRouter.get('/get-all-not-available-cars',getAllNotAvailableCars);
 carRouter.get('/get-car-by-id/:id',getCarById);
 
 module.exports = carRouter;
