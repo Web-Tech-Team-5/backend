ba4169decd0a190ec358620dc7ad65ac57e182df
AdilMulimani <adilmulimani@gmail.com>
2025-01-01 19:03:28 +0530

    feat(config): add test condition for database.js

--- config/database.js
+++ config/database.js
@@ -3,7 +3,10 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const connectDatabase = async () => {
-    await mongoose.connect(process.env.MONGODB_URI);
+    // Connect to database
+    if (process.env.NODE_ENV !== 'test') {
+        await mongoose.connect(process.env.MONGODB_URI);
+    }
 }
 
 module.exports = connectDatabase;
